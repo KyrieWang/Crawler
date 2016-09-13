e44d62470dada3ab2347bb7b3beda1b94f8f2342
BinWang <747910316@qq.com>
2016-09-13 21:38:41 +0800

    hehe

--- src/confparser.cpp
+++ src/confparser.cpp
@@ -1,4 +1,6 @@
 #include <memory>
+#include <fstream>
+#include <string>
 #include "confparser.h"
 
 using namespace std;
@@ -7,40 +9,47 @@ Confparser::Confparser(){
 
 }
 
-shared_ptr<Confparser> Confparser::getInstance(){
+shared_ptr<Confparser> Confparser::getInstance()
+{
 	if (self_ptr == NULL)
 		self_ptr = make_shared<Confparser>();
 	return self_ptr;
 }
 
-int Confparser::loader(char* conf_filepath){
+int Confparser::loader(const string& conf_filepath)
+{
+	const string& delim = m_Delimiter;
+	const string& comm = m_Comment;
+
+	ifstream in(conf_filepath);
+
+	if(in)
+		return 1;
+
+	while(!in.eof())
+	{
+		string line;
+		
+		getline(in, line);
+		line = line.substr(0, line.find(comm));
+		size_type delim_pos = line.find(delim);
+
+		if (pos != npos)
+		{
+			string key = line.substr(0, delim_pos);
+		}
+	}
+}
+
+CONF_FILE* Confparser::getConfFile()
+{
+	return &conf_file;
+}
+
+void Confparser::Trim()
+{
 
 }
 
-int Confparser::getJobNum(){
 
-}
 
-int Confparser::getDeep(){
-
-}
-
-int Confparser::getLogLevel(){
-
-}
-
-char Confparser::getUrlSeed(){
-
-}
-
-char* Confparser::getModulePath(){
-
-}
-
-vector<string> Confparser::getModules(){
-
-}
-
-vector<string> Confparser::getFileTypes(){
-
-}
--- src/confparser.h
+++ src/confparser.h
@@ -7,34 +7,61 @@
 
 using namespace std;
 
-struct stru_confFile
+typedef struct stru_confFile
 {
 	int job_num; //并发任务数
 	int deeps; //抓取深度
 	int log_level; //输出日志等级
-	char* seed; //url种子
-	char* module_path; //模块存放路径
+	string seed; //url种子
+	string module_path; //模块存放路径
 	vector<string> module_name; //模块名
 	vector<string> file_type; //抓取的资源类型
-};
+}CONF_FILE;
 
 /*
-*配置文件读取模块
+*配置文件读取模块，单例模式
 */
 class Confparser
 {
+protected:
+	string m_Delimiter = "=";
+	string m_Comment = "#";
+	map<string, string> m_Contents; //存储key和value
+
 public:
 	shared_ptr<Confparser> getInstance();
 
-	int loader(const char* conf_filepath); //读取配置文件
-	struct stru_confFile* getConfFile();
+	int Loader(const char* conf_filepath); //读取配置文件
+	CONFFILE* getConfFile();
+
+private:
+	template<class T>
+	T Read(const string& in_key, const T& in_value) const;
+
+	template<class T>
+	T String_to_T(const string& s)const;
+
+	void Trim(string& inout_s); //去掉首尾的空格
+
 
 private:
 	Confparser();
 
 	static shared_ptr<Confparser> self_ptr;
 
-	struct stru_confFile conf_file;
+	CONF_FILE conf_file;
 };
+
+template<class T>
+T Confparser::Read(const string& in_key, const T& in_value)
+{
+
+}
+
+template<class T>
+T Confparser::String_to_T(const string& s)
+{
+	
+}
 
 #endif
