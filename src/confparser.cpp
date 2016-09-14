023e4aa28560264e3eca4c601bb67a1ae11c05d5
BinWang <747910316@qq.com>
2016-09-14 16:45:27 +0800

    完成读取配置文件类

--- src/confparser.cpp
+++ src/confparser.cpp
@@ -20,6 +20,7 @@ int Confparser::loader(const string& conf_filepath)
 {
 	const string& delim = m_Delimiter;
 	const string& comm = m_Comment;
+	const size_type skip = delim.length();
 
 	ifstream in(conf_filepath);
 
@@ -31,14 +32,28 @@ int Confparser::loader(const string& conf_filepath)
 		string line;
 		
 		getline(in, line);
-		line = line.substr(0, line.find(comm));
+		line = line.substr(0, line.find(comm)); //去注释
 		size_type delim_pos = line.find(delim);
 
-		if (pos != npos)
+		if (delim_pos != npos)
 		{
 			string key = line.substr(0, delim_pos);
+			string value = line.replace(0, delim_pos+skip, "");
+
+			Trim(key);
+			Trim(value)
+
+			m_Contents[key] = value;
 		}
 	}
+
+	conf_file.job_num = Read("job_num", conf_file.job_num);
+	conf_file.deeps = Read("deeps", conf_file.deeps);
+	conf_file.log_level = Read("log_level", conf_file.log_level);
+	conf_file.seed = Read("seed", conf_file.seed);
+	conf_file.module_path = Read("module_path", conf_file.module_path);
+	conf_file.module_names = Read_Vec("module_names", conf_file.module_names);
+	conf_file.file_types = Read_Vec("file_types", conf_file.file_types);
 }
 
 CONF_FILE* Confparser::getConfFile()
@@ -46,9 +61,21 @@ CONF_FILE* Confparser::getConfFile()
 	return &conf_file;
 }
 
-void Confparser::Trim()
+void Confparser::Trim(string& str)
 {
+	string space = " ";
 
+	size_type begin_pos = str.find_first_not_of(space);
+	if (begin_pos != npos)
+	{
+		str.erase(0, begin_pos);
+	}
+
+	size_type end_pos = str.find_last_not_of(space);
+	if (end_pos != npos)
+	{
+		str.erase(str.begin()+end_pos+1, str.end());
+	}
 }
 
 
--- src/confparser.h
+++ src/confparser.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -14,8 +15,8 @@ typedef struct stru_confFile
 	int log_level; //输出日志等级
 	string seed; //url种子
 	string module_path; //模块存放路径
-	vector<string> module_name; //模块名
-	vector<string> file_type; //抓取的资源类型
+	vector<string> module_names; //模块名
+	vector<string> file_types; //抓取的资源类型
 }CONF_FILE;
 
 /*
@@ -32,16 +33,19 @@ public:
 	shared_ptr<Confparser> getInstance();
 
 	int Loader(const char* conf_filepath); //读取配置文件
-	CONFFILE* getConfFile();
+	CONF_FILE* getConfFile();
 
 private:
 	template<class T>
 	T Read(const string& in_key, const T& in_value) const;
 
 	template<class T>
-	T String_to_T(const string& s)const;
+	T Read_Vec(const string& in_key, const T& vec) const;
 
-	void Trim(string& inout_s); //去掉首尾的空格
+	template<class T>
+	T String_to_T(const string& str);
+
+	void Trim(string& str); //去掉首尾的空格
 
 
 private:
@@ -53,15 +57,59 @@ private:
 };
 
 template<class T>
-T Confparser::Read(const string& in_key, const T& in_value)
+T Confparser::Read(const string& in_key, const T& in_value) const
 {
+	map<string, string>::const_iterator mapci = m_Contents.find(in_key);
 
+	if(mapci == m_Contents.end() )
+		return in_value;
+
+	return String_to_T<T>(mapci->second);
 }
 
 template<class T>
-T Confparser::String_to_T(const string& s)
+T Confparser::Read_Vec(const string& in_key, const T& vec ) const
 {
+	map<string, string>::const_iterator mapci = m_Contents.find(in_key);
+
+	string line = mapci->second;
+	string in_line;
+	string comma = ",";
 	
+	size_type pos = line.find(comma);
+	size_type skip = comma.length();
+
+	if (pos != npos)
+	{
+		while(pos ！= npos)
+		{
+			in_line = line.substr(0, pos);
+			vec.push_back(in_line);
+			line = line.replace(0, pos+skip, "");
+			pos = line.find(comma);
+		}
+		vec.push_back(line);
+	}
+	else vec.push_back(line);
+
+	return vec;
 }
+
+template<class T>
+T Confparser::String_to_T(const string& str)
+{
+	T t;
+	istringstream ist(str);
+	ist >> t;
+	return t;
+}
+
+/*
+template<class T>
+string Confparser::String_to_T<string>(const string& str)
+{
+	return str;
+}
+*/
 
 #endif
