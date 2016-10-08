d1b1c76b8d89a0f38c194f354229227fa31a559d
BinWang <747910316@qq.com>
2016-10-08 16:30:20 +0800

    nothing

--- src/sock.h
+++ src/sock.h
@@ -4,6 +4,21 @@
 #include <socket.h>
 #include "http.h"
 
+/*http响应*/
+typedef struct http_response
+{
+	ResHead *head_ptr;
+	char *content;
+	int cont_len;
+}Response;
+
+/*http响应头*/
+typedef struct response_head
+{
+	char *cont_type;
+	int sta_code;
+}ResHead;
+
 class Socket
 {
 public:
@@ -13,15 +28,14 @@ public:
 	int build_connect(Url* url);
 	int dis_connect();
 	int send_request(Url* url);
-	int parse_response(Response* resp_ptr);
-//	int setSocketMod()
+	int recv_response(std::deque<Url*> src_urldeq);
 
 private:
+	int save_content();
+	int parse_head();
 
 private:
 	int sock_fd; //句柄
-//	Http m_http;
-	char* buf;
 };
 
 #endif
